43ca84983b42d1d1d13ac60e5d34edb0ba9ee687
zchen2263 <zchen2263@wisc.edu>
2026-02-20 14:51:07 -0600

    control.v

---
+++ project3/rtl/control.v
@@ -0,0 +1,13 @@
+module control(
+    input  wire [31:0] i_inst,
+    output wire o_rd_wen,
+    output wire [2:0] o_opsel,
+    output wire o_sub,
+    output wire o_unsigned,
+    output wire o_arith,
+    output wire o_mem_wen,
+    output wire o_men_to_reg,
+    output wire o_alu_src_immediate,
+);
+
+endmodule
