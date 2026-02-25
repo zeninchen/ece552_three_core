7dde1014f613f35040077ff6396b0005bedfac97
Yanwen-Gu <ygu040624@gmail.com>
2026-02-25 10:44:54 -0600

    Add files via upload

--- project3/rtl/branch_decoder.v
+++ project3/rtl/branch_decoder.v
@@ -3,7 +3,14 @@ module branch_decoder (
     input  wire is_branch,
     input wire eq,
     input wire slt,
-    output wire        b_sel
+    output wire b_sel
 );
     //TODO
+    assign b_sel = (is_branch && ((funct3 == 3'b000 && eq) || // beq
+                              (funct3 == 3'b001 && !eq) || // bne
+                              (funct3 == 3'b100 && slt) || // blt
+                              (funct3 == 3'b101 && !slt) || // bge
+                              (funct3 == 3'b110 && slt) || // bltu
+                              (funct3 == 3'b111 && !slt))); // bgeu
+
 endmodule
